2daffdb3ffb53fb2423b627d501a3c62c52937c8
mef1991fonseca <51392399+mef1991fonseca@users.noreply.github.com>
2022-10-05 17:47:43 -0300

    Add files via upload

--- contenedorProductos.js
+++ contenedorProductos.js
@@ -12,28 +12,6 @@ class Contenedor{
         return arrayProductos
     }
 
-    //getById(Number): Object - Recibe un id y devuelve el objeto con ese id, o null si no está.
-    /*getById = async(id) =>{
-       try {
-            if(fs.existsSync(this.nombreArchivo)){
-                const contenido = await fs.promises.readFile(this.nombreArchivo, "utf-8")
-                if (contenido){
-                    const arrayProductos = JSON.parse(contenido)
-                    const producto = arrayProductos.find(item=>item.id === id)
-                    if(producto){
-                        return producto
-                    }
-                    else{
-                        return "Producto no encontrado"
-                    }
-                }else{
-                    return "El archivo está vacio"
-                }
-            }
-       } catch (error) {
-        
-       }
-    }*/
     getById = async(id) =>{
         const contenido = await fs.promises.readFile(this.nombreArchivo, "utf-8")
         const arrayProductos = JSON.parse(contenido)
